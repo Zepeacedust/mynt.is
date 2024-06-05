78a55401b187b9074f764bf6af578486cd616a21
Zepeacedust <arnor0903@gmail.com>
2024-06-05 21:47:03 +0000

    add News via Django

--- mynt/news/models.py
+++ mynt/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from markdownx.models import MarkdownxField
+
 # Create your models here.
+
+    
+class Profile(models.Model):
+    name = models.CharField(max_length=32)
+    desc = MarkdownxField()
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=32)
+    content = MarkdownxField()
+    
